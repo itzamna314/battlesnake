499d6f673b7d41330bfa30945a9b1648ccd4f1ed
Kyl Wellman <KylWellman@BoaWeb.com>
2021-09-19 21:06:27 +0000

    customizations

--- logic.go
+++ logic.go
@@ -19,10 +19,10 @@ func info() BattlesnakeInfoResponse {
 	log.Println("INFO")
 	return BattlesnakeInfoResponse{
 		APIVersion: "1",
-		Author:     "",        // TODO: Your Battlesnake username
-		Color:      "#888888", // TODO: Personalize
-		Head:       "default", // TODO: Personalize
-		Tail:       "default", // TODO: Personalize
+		Author:     "itzamna",  
+		Color:      "#FF4F00", 
+		Head:       "fang", 
+		Tail:       "bolt", 
 	}
 }
 
