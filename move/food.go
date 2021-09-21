97da66b47a79bdebb8722bc0ea0b39ce4fa24bad
Kyl Wellman <itsawmna314@gmail.com>
2021-09-20 21:25:44 -0600

    :snake: Only eat when we need to

--- move/food.go
+++ move/food.go
@@ -5,6 +5,12 @@ import "github.com/itzamna314/battlesnake/model"
 const FoodWeight = 0.15
 
 func weightFood(state *model.GameState, coord *model.Coord) float64 {
+	foodWeight := FoodWeight
+
+	if !wantFood(state) {
+		weight *= -1
+	}
+
 	// Find closest food to our head
 	var (
 		closestFoods []model.Coord
@@ -35,9 +41,29 @@ func weightFood(state *model.GameState, coord *model.Coord) float64 {
 		myDist := coord.Dist(&food)
 		if myDist < minDist {
 			amtCloser := float64(minDist-myDist) / float64(minDist)
-			weight = weight + FoodWeight*amtCloser
+			weight = weight + foodWeight*amtCloser
 		}
 	}
 
 	return weight / float64(len(closestFoods))
+}
+
+func wantFood(state *model.GameState) bool {
+	if state.You.Health < 20 {
+		return true
+	}
+
+	var myLen = state.You.Length
+
+	for _, snake := range state.Snakes {
+		if snake.ID == state.You.ID {
+			continue
+		}
+
+		if snake.Length >= state.You.Length {
+			return true
+		}
+	}
+
+	return false
 }
